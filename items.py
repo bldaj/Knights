439fd65e6c63d48ff3bc03fb4d50dcf2c28c861e
K <wetarmor@gmail.com>
2017-05-06 17:02:40 +0300

    added class named Weapon

--- items.py
+++ items.py
@@ -14,7 +14,7 @@ class Food():
 
 
 class Potion():
-    def __init__(self, name, description,value, cost):
+    def __init__(self, name, description, value, cost):
         self.name = name
         self.description = description
         self.value = value
@@ -22,9 +22,23 @@ class Potion():
         # Name of potion can be only 'Health potion' or 'Energy potion'
 
 
+class Weapon():
+    def __init__(self, name, description, damage, weapon_kind, weapon_type, requirement_level, cost):
+        self.name = name
+        self.description = description
+        self.damage = damage
+        self.weapon_kind = weapon_kind  # sword, dagger, mace and etc.
+        self.weapon_type = weapon_type  # one-handed or two-handed
+        self.requirement_level = requirement_level
+        self.cost = cost
+
+
 carrot = Food('Carrot', 'Crispy vegetable', 1, 3)
+
 health_potion = Potion('Health potion', 'This potion is healing your health', 20, 5)
 energy_potion = Potion('Energy potion', 'This potion is restoring your energy', 20, 5)
 
+sword = Weapon('Simple sword', 'Sharps as your life', 30, 'sword', 'one-handed', '1', 15)
 
-trader = [health_potion, energy_potion]
+
+trader = [health_potion, energy_potion, sword, carrot]
