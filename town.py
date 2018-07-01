79949f6f14ee208d474b265c6de0ee10d649e205
Konstantin <wetarmor@gmail.com>
2018-07-01 18:47:15 +0300

    Added Main menu to the Town

--- town.py
+++ town.py
@@ -1,4 +1,46 @@
+import pickle
 from utils import *
+
+
+def save_game(hero):
+    with open('save.pickle', 'wb') as f:
+        pickle.dump(hero, f)
+
+
+def load_game(hero):
+    with open('save.pickle', 'rb') as f:
+        loaded_hero = pickle.load(f)
+
+    hero.name = loaded_hero.name
+    hero.health = loaded_hero.health
+    hero.energy = loaded_hero.energy
+    hero.gold = loaded_hero.gold
+    hero.exp = loaded_hero.exp
+    hero.level = loaded_hero.level
+
+    return hero
+
+
+def main_menu(hero):
+    display_title('Main menu')
+
+    commands = ['Resume', 'Load game', 'Save game', 'Exit game']
+
+    display_commands(commands)
+    cmd = get_cmd()
+
+    if cmd == '1':
+        return
+    elif cmd == '2':
+        hero = load_game(hero)
+        return hero
+    elif cmd == '3':
+        save_game(hero)
+    elif cmd == '4':
+        exit('Game exit')
+    else:
+        display_incorrect_command()
+        main_menu(hero)
 
 
 def town_menu(hero, enemies: list):
@@ -11,11 +53,10 @@ def town_menu(hero, enemies: list):
 
     commands = ['Main menu']
 
-    display_commands(commands)
+    while True:
+        display_commands(commands)
 
-    cmd = get_cmd()
+        cmd = get_cmd()
 
-    if cmd == '1':
-        from main import main_menu
-
-        main_menu()
+        if cmd == '1':
+            main_menu(hero)
