78651de0b3a5a084c209109639f1e27c10f1e01d
k <wetarmor@gmail.com>
2018-07-06 23:41:57 +0300

    Changed gold and experience values

--- enemies.py
+++ enemies.py
@@ -24,10 +24,10 @@ class Enemy:
 def create_enemies_list():
     enemies = []
 
-    dummy = Enemy(name='Dummy', health=60, energy=100, gold=10, exp=60)
-    villager = Enemy(name='Villager', health=80, energy=100, gold=20, exp=100)
-    farmer = Enemy(name='Farmer', health=100, energy=100, gold=30, exp=150)
-    knight = Enemy(name='Knight', health=140, energy=120, gold=50, exp=280)
+    dummy = Enemy(name='Dummy', health=60, energy=100, gold=5, exp=60)
+    villager = Enemy(name='Villager', health=80, energy=100, gold=10, exp=100)
+    farmer = Enemy(name='Farmer', health=100, energy=100, gold=10, exp=120)
+    knight = Enemy(name='Knight', health=140, energy=120, gold=15, exp=210)
 
     enemies.extend((dummy, villager, farmer, knight))
 
