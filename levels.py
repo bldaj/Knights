7708c97cb556ee355b2d0fa400f2b830afc5c11a
k <wetarmor@gmail.com>
2018-07-22 20:26:08 +0300

    Increased exp values

--- levels.py
+++ levels.py
@@ -1,5 +1,5 @@
 # [0, 101, 204, 309, 416, 525, 636, 749, 864, 981, 1100, 1221, 1344, 1469, 1596, 1725, 1856, 1989, 2124, 2261]
-LEVELS = [(exp + 100) * k for k, exp in enumerate(range(20))]
+LEVELS = [round((exp + 100) * k * 1.5) for k, exp in enumerate(range(20))]
 
 
 if __name__ == '__main__':
