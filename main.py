345f7d0678aa0ff332c2bd921f511268af0e61a5
k <wetarmor@gmail.com>
2018-08-02 00:01:47 +0300

    Fixed loading game

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def load_game():
         hero.intelligence = loaded_hero.intelligence
         hero.physical_resistance = loaded_hero.physical_resistance
         hero.magical_resistance = loaded_hero.magical_resistance
-        hero.attribute_points = loaded_hero.stat_points
+        hero.attribute_points = loaded_hero.attribute_points
         hero.speed_attack = loaded_hero.speed_attack
         hero.inventory = loaded_hero.inventory
 
