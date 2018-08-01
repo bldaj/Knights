47bee624ce81b9dfc54cbab3ce9aa1b41cf07730
k <wetarmor@gmail.com>
2018-08-01 08:18:29 +0300

    Updated taking a rest

--- rest.py
+++ rest.py
@@ -1,2 +1,18 @@
+from utils import display_commands, get_cmd, display_incorrect_command
+
+
 def take_a_rest(hero):
-    pass
+    while True:
+        display_commands(commands=['Stay at low level hotel', 'Stay at middle level hotel', 'Stay at high level hotel',
+                                   'Back to the previous menu'])
+
+        cmd = get_cmd()
+
+        if cmd == '1':
+            pass
+        elif cmd == '2':
+            pass
+        elif cmd == '3':
+            pass
+        elif cmd == '4':
+            return
