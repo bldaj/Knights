2412350f436d6108d184dbd9b8997fc986e4b733
k <wetarmor@gmail.com>
2022-05-09 01:33:37 +0300

    Added basic character attrs. Added base Hero class

--- characters/common.py
+++ characters/common.py
@@ -5,6 +5,16 @@ class BaseCharacter(abc.ABC):
     """
     """
 
+    def __init__(
+            self,
+            name: str,
+            health: int,
+            energy: int,
+    ):
+        self.name = name
+        self.health = health
+        self.energy = energy
+
     @abc.abstractmethod
     def attack(self):
         pass
---
+++ characters/hero.py
@@ -0,0 +1,13 @@
+from characters.common import (
+    BaseCharacter,
+)
+
+
+class Hero(BaseCharacter):
+    """
+    """
+
+    def attack(self):
+        """
+        """
+        pass
